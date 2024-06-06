a48daddca20a59577491b7b1f2a873236be63ccb
Roy Mayan <>
2024-06-06 18:30:39 +0300

    Added go crawler

--- interop/crawler.py
+++ interop/crawler.py
@@ -5,6 +5,7 @@ def extract_links_from_url(url: str, depth: int) -> list:
     links = []
     if depth == 0:
         return links
+    
     response = requests.get(url)
     soup = BeautifulSoup(response.text,'html.parser')
     for link in soup.find_all('a'):
@@ -12,4 +13,5 @@ def extract_links_from_url(url: str, depth: int) -> list:
         if href and href.startswith('http'):
             links.append(href)
             links.extend(extract_links_from_url(href, depth - 1))
+    
     return links

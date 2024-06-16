c982eb13c24e0261497ff172bf293f60659bbf7d
Roy Mayan <>
2024-06-16 12:28:01 +0300

    test passed

--- services/test-service/service/TestService.go
+++ services/test-service/service/TestService.go
@@ -1,9 +1,14 @@
 package TestService
 
 import (
+	"context"
+
 	services "github.com/TAULargeScaleWorkshop/RLAD/services/common"       // import common as services
 	. "github.com/TAULargeScaleWorkshop/RLAD/services/test-service/common" // from test-service/common import *
+	. "github.com/TAULargeScaleWorkshop/RLAD/utils"                        // from utils import *
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type testServiceImplementation struct {
@@ -15,4 +20,9 @@ func Start(configData []byte) error {
 		RegisterTestServiceServer(s, &testServiceImplementation{})
 	}
 	services.Start("TestService", 50051, bindgRPCToService)
+}
+
+func (obj *testServiceImplementation) HelloWorld(_ context.Context, _ *emptypb.Empty) (res *wrapperspb.StringValue, err error) {
+	Logger.Printf("HelloWorld called")
+	return wrapperspb.String("Hello World"), nil
 }

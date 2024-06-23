1ce3e03d49b83f91927305c49e3697cefffe4131
Roy Mayan <>
2024-06-23 12:48:19 +0300

    Removed TODO

--- services/common/ServiceBase.go
+++ services/common/ServiceBase.go
@@ -24,7 +24,7 @@ func startgRPC(listenPort int) (listeningAddress string, grpcServer *grpc.Server
 }
 
 func Start(serviceName string, grpcListenPort int, bindgRPCToService func(s grpc.ServiceRegistrar)) {
-	_, grpcServer, startListening := startgRPC(grpcListenPort) // TODO: First param was listeningAddress
+	_, grpcServer, startListening := startgRPC(grpcListenPort)
 	bindgRPCToService(grpcServer)
 	startListening()
 }

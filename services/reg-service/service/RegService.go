61ffc953d8410e8562feb04aac9641f2a8dd668a
Avishay Spitzer <avishays@mail.tau.ac.il>
2024-06-30 10:00:27 +0000

    add network layer for reg service for Register, Unregister and Discover

--- services/reg-service/service/RegService.go
+++ services/reg-service/service/RegService.go
@@ -1,26 +1,45 @@
-package TestService
+package RegService
 
 import (
 	"context"
 
-	services "github.com/TAULargeScaleWorkshop/RLAD/services/common"       // import common as services
-	// . "github.com/TAULargeScaleWorkshop/RLAD/services/test-service/common" // from test-service/common import *
-	// RegistryServiceServant "github.com/TAULargeScaleWorkshop/RLAD/services/test-service/servant"
-	// . "github.com/TAULargeScaleWorkshop/RLAD/utils" // from utils import *
+	services "github.com/TAULargeScaleWorkshop/RLAD/services/common"
+	. "github.com/TAULargeScaleWorkshop/RLAD/services/reg-service/common"
+	RegServiceServant "github.com/TAULargeScaleWorkshop/RLAD/services/reg-service/servant"
+	. "github.com/TAULargeScaleWorkshop/RLAD/utils"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
-type testServiceImplementation struct {
-	UnimplementedTestServiceServer
+type regServiceImplementation struct {
+	UnimplementedRegServiceServer
 }
 
 func Start(configData []byte) error {
 	bindgRPCToService := func(s grpc.ServiceRegistrar) {
-		RegTestServiceServer(s, &regServiceImplementation{})
+		RegisterRegServiceServer(s, &regServiceImplementation{})
 	}
 	services.Start("RegistryService", 8502, bindgRPCToService)
 	return nil
 }
+
+func (obj *regServiceImplementation) Register(_ context.Context, params *UpdateRegistryParameters) (_ *emptypb.Empty, err error) {
+	Logger.Printf("Register called with service: %s, address: %s", params.ServiceName, params.NodeAddr)
+	RegServiceServant.Register(params.ServiceName, params.NodeAddr)
+	return &emptypb.Empty{}, nil
+}
+
+func (obj *regServiceImplementation) Unregister(_ context.Context, params *UpdateRegistryParameters) (_ *emptypb.Empty, err error) {
+	Logger.Printf("Unregister called with service: %s, address: %s", params.ServiceName, params.NodeAddr)
+	RegServiceServant.Unregister(params.ServiceName, params.NodeAddr)
+	return &emptypb.Empty{}, nil
+}
+
+func (obj *regServiceImplementation) Discover(_ context.Context, service_name *wrapperspb.StringValue) (*DiscoveredServers, error) {
+	Logger.Printf("Discover called with service: %s", service_name.Value)
+	value, err := RegServiceServant.Discover(service_name.Value)
+	return &DiscoveredServers{Addresses: value}, err
+}
+

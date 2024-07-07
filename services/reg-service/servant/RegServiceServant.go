bd0d50a7c1597c0851df112da67a94a6b24b1dc4
avishays-tau <157728644+avishays-tau@users.noreply.github.com>
2024-07-07 15:11:16 +0300

    maintain lists of nodes instead of strings

--- services/reg-service/servant/RegServiceServant.go
+++ services/reg-service/servant/RegServiceServant.go
@@ -2,44 +2,44 @@ package RegServiceServant
 
 import (
 	"fmt"
-	"sync"
-	"math/rand"
 	"time"
 
-	metaffi "github.com/MetaFFI/lang-plugin-go/api"
-	"github.com/MetaFFI/plugin-sdk/compiler/go/IDL"
+	. "github.com/TAULargeScaleWorkshop/RLAD/services/test-service/client"
 	"github.com/TAULargeScaleWorkshop/RLAD/utils"
 )
 
 var (
-    cacheMap map[string][]string
-    fails    map[string]int
-    mu       sync.Mutex
+	cacheMap map[string][]Node
 )
 
+type Node struct {
+	Address   string
+	FailCount int
+	Alive     bool
+}
+
 func init() {
-	cacheMap = make(map[string][]string)
-	fails = make(map[string]int)
+	cacheMap = make(map[string][]Node)
 
 }
 
 func Register(service_name string, node_address string) {
 	if cacheMap[service_name] != nil {
-		for _, addr := range cacheMap[service_name] {
-			if addr == node_address {
+		for _, node := range cacheMap[service_name] {
+			if node.Address == node_address {
 				utils.Logger.Printf("Address %s already exists for service %s\n", node_address, service_name)
 				return
 			}
 		}
 	}
-	cacheMap[service_name] = append(cacheMap[service_name], node_address)
+	cacheMap[service_name] = append(cacheMap[service_name], Node{Address: node_address, FailCount: 0, Alive: true})
 	utils.Logger.Printf("Address %s added for service %s\n", node_address, service_name)
 }
 
 func Unregister(service_name string, node_address string) {
 	if cacheMap[service_name] != nil {
-		for i, addr := range cacheMap[service_name] {
-			if addr == node_address {
+		for i, node := range cacheMap[service_name] {
+			if node.Address == node_address {
 				cacheMap[service_name] = append(cacheMap[service_name][:i], cacheMap[service_name][i+1:]...)
 				utils.Logger.Printf("Address %s removed for service %s\n", node_address, service_name)
 				return
@@ -52,40 +52,44 @@ func Unregister(service_name string, node_address string) {
 }
 
 func Discover(service_name string) ([]string, error) {
-	value, ok := cacheMap[service_name]
+	nodes, ok := cacheMap[service_name]
+	addresses := []string{}
 	if !ok {
-		return value, fmt.Errorf("service not found: %v", service_name)
+		return addresses, fmt.Errorf("service not found: %v", service_name)
 	}
-	return value, nil
+	for _, node := range nodes {
+		addresses = append(addresses, node.Address)
+	}
+	return addresses, nil
 }
-//TODO:implement IsAlive
+
 func IsAliveCheck() {
-    ticker := time.NewTicker(10 * time.Second)
-    defer ticker.Stop()
-    for range ticker.C {
-        for serviceName, addresses := range cacheMap {
-            for _, address := range addresses {
-				mu.Lock()
-                alive, err := IsAlive(address)
-                if err != nil {
-                    utils.Logger.Printf("IsAlive check failed for address %s: %v\n", address, err)
-					mu.Unlock()
-					continue
-                } else if !alive {
-                    count := fails[address]
-                    count++
-                    if count >= 2 {
-                        Unregister(serviceName, address)
-                        delete(fails, address)
-                        utils.Logger.Printf("Node %s unregistered from service\n", address, serviceName)
-                    } else {
-                        fails[address] = count
-                    }
-                } else {
-                    delete(fails, address)
-                }
-				mu.Unlock()
-            }
-        }
-    }
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	for range ticker.C {
+		for serviceName, nodes := range cacheMap {
+			for i, node := range nodes {
+				c := NewTestServiceClient(node.Address)
+				alive, err := c.IsAlive()
+				// we assume that (!alive) iff (err != nil) in IsAlive implementation
+				if !alive {
+					utils.Logger.Printf("Service is not alive, address %s: %v\n", node.Address, err)
+					nodes[i].FailCount++
+					if node.FailCount >= 2 {
+						// mark the node to be unregistered later
+						nodes[i].Alive = false
+					}
+				} else {
+					nodes[i].FailCount = 0
+					nodes[i].Alive = true
+				}
+			}
+			// unregister the "dead" nodes
+			for i := len(nodes); i >= 0; i-- {
+				if !nodes[i].Alive {
+					Unregister(serviceName, nodes[i].Address)
+				}
+			}
+		}
+	}
 }

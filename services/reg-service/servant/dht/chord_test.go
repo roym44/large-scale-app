2a03f78c6116dd6ed851bb2181824e735eddfa4c
Lion Vasiliev <lionvasiliev@mail.tau.ac.il>
2024-07-13 18:46:46 +0300

    added and implemented dht functions and test

---
+++ services/reg-service/servant/dht/chord_test.go
@@ -0,0 +1,76 @@
+package dht
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	//newChord
+	chord, err := NewChord("node1", 1099)
+	if err != nil {
+		t.Fatalf("could not create new chord: %v", err)
+		return
+	}
+	t.Logf("Response: %v", chord)
+	//joinChord
+	j_chord, err := JoinChord("node2", "node1", 1099)
+	if err != nil {
+		t.Fatalf("could not join chord: %v", err)
+		return
+	}
+	t.Logf("Response: %v", j_chord)
+	//set
+	err = chord.Set("key1", "value1")
+	if err != nil {
+		t.Fatalf("could not call Set: %v", err)
+		return
+	}
+	//get
+	r, err := chord.Get("key1")
+	if err != nil {
+		t.Fatalf("could not call Get: %v", err)
+		return
+	}
+	if r != "value1" {
+		t.Fatalf("wrong value: received %s, expected value1", r)
+		return
+	}
+	t.Logf("Response: %v", r)
+	//delete
+	err = chord.Delete("key1")
+	if err != nil {
+		t.Fatalf("could not call delete: %v", err)
+		return
+	}
+	//getAllKeys
+	err = chord.Set("key1", "value1")
+	if err != nil {
+		t.Fatalf("could not call Set: %v", err)
+		return
+	}
+	err = chord.Set("key2", "value2")
+	if err != nil {
+		t.Fatalf("could not call Set: %v", err)
+		return
+	}
+	keys, err := chord.GetAllKeys()
+	if len(keys) != 2 {
+		t.Fatalf("wrong value: received %d, expected 2", len(keys))
+		return
+	}
+	t.Logf("Response: %v", keys)
+	//IsFirst
+	is_first, err := chord.IsFirst()
+	if err != nil {
+		t.Fatalf("could not call IsFirst: %v", err)
+		return
+	}
+	if !is_first {
+		t.Fatalf("not first as expected")
+	}
+
+}
+
+
+
+

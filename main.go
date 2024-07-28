5294e6a5139308c58946456be0c19bec61442424
Dory Grossman (GitHub2) <dorygrossman@mail.tau.ac.il>
2024-07-28 11:09:10 +0000

    added cache to main

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/TAULargeScaleWorkshop/RLAD/config"
 	RegService "github.com/TAULargeScaleWorkshop/RLAD/services/reg-service/service"
 	TestService "github.com/TAULargeScaleWorkshop/RLAD/services/test-service/service"
+	CacheService "github.com/TAULargeScaleWorkshop/RLAD/services/cache-service/service"
+
 	"github.com/TAULargeScaleWorkshop/RLAD/utils"
 	"gopkg.in/yaml.v2"
 )
@@ -30,14 +32,14 @@ func main() {
 		log.Fatalf("error unmarshaling data: %v", err)
 		os.Exit(3)
 	}
-
+	utils.Logger.Printf("Loading service type: %v\n", config.Type)
 	switch config.Type {
 	case "TestService":
-		utils.Logger.Printf("Loading service type: %v\n", config.Type)
 		TestService.Start(configData)
 	case "RegService":
-		utils.Logger.Printf("Loading service type: %v\n", config.Type)
 		RegService.Start(configData)
+	case "CacheService":
+		CacheService.Start(configData)
 	default:
 		utils.Logger.Fatalf("Unknown configuration type: %v", config.Type)
 		os.Exit(4)

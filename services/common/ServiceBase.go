f34bddd03d63ff2bd2b02aeaae28fc8e495847a8
Roy Mayan <>
2024-08-04 12:59:36 +0300

    Implemented MQ support server side

--- services/common/ServiceBase.go
+++ services/common/ServiceBase.go
@@ -6,8 +6,82 @@ import (
 
 	RegServiceClient "github.com/TAULargeScaleWorkshop/RLAD/services/reg-service/client"
 	. "github.com/TAULargeScaleWorkshop/RLAD/utils"
+	"github.com/pebbe/zmq4"
+	"go.starlark.net/lib/proto"
 	"google.golang.org/grpc"
 )
+
+func bindMQToService(listenPort int, messageHandler func(method string,
+	parameters []byte) (response proto.Message, err error)) (startMQ func(), listeningAddress string) {
+	socket, err := zmq4.NewSocket(zmq4.REP)
+	if err != nil {
+		Logger.Fatalf("Failed to create a new zmq socket: %v", err)
+	}
+	if listenPort == 0 {
+		listeningAddress = "tcp://127.0.0.1:*"
+	} else {
+		listeningAddress = fmt.Sprintf("tcp://127.0.0.1:%v", listenPort)
+	}
+	err = socket.Bind(listeningAddress)
+	if err != nil {
+		Logger.Fatalf("Failed to bind a zmq socket: %v", err)
+	}
+	listeningAddress, err = socket.GetLastEndpoint()
+	if err != nil {
+		Logger.Fatalf("Failed to get listetning address of zmq socket: %v", err)
+	}
+
+	startMQ = func() {
+		for {
+			data, readerr := socket.RecvBytes(0)
+			if err != nil {
+				Logger.Printf("Failed to receive bytes from MQ socket: %v\n", readerr)
+				continue
+			}
+			if len(data) == 0 {
+				Logger.Printf("No data to process")
+				continue
+			}
+			Logger.Printf("data len: %v\n", len(data))
+
+			// handle the request in a new goroutine
+			go func() {
+				callParams := &CallParameters{}
+				err := proto.Unmarshal(data, callParams)
+				if err != nil {
+					Logger.Printf("Unmarshal failed: %v\n", err)
+					return
+				}
+				response, err := messageHandler(callParams.Method, callParams.Data)
+				returnValue := &ReturnValue{}
+				if err != nil {
+					Logger.Printf("messageHandler failed with: %v\n", err)
+					returnValue.Error = err.Error() // get the string for the error
+				} else { // no error, get the data
+					returnValue.Data, err = proto.Marshal(response)
+					if err != nil {
+						Logger.Printf("Marshal (1) failed: %v\n", err)
+						returnValue.Error = err.Error()
+					}
+				}
+
+				// process the full returnValue (data + err)
+				responseData, err := proto.Marshal(returnValue)
+				if err != nil {
+					Logger.Printf("Marshal (2) failed: %v\n", err)
+					return
+				}
+
+				_, senderr := socket.SendBytes(responseData, 0)
+				if senderr != nil {
+					Logger.Printf("SendBytes failed: %v\n", err)
+					return
+				}
+			}()
+		}
+	}
+	return startMQ, listeningAddress
+}
 
 func startgRPC(listenPort int) (listeningAddress string, grpcServer *grpc.Server, startListening func(), err error) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", listenPort))
@@ -26,7 +100,8 @@ func startgRPC(listenPort int) (listeningAddress string, grpcServer *grpc.Server
 }
 
 // regAddresses - the registry service addresses to connect to (for all the services who aren't the registry)
-func Start(serviceName string, grpcListenPort int, regAddresses []string, bindgRPCToService func(s grpc.ServiceRegistrar)) (err error) {
+func Start(serviceName string, grpcListenPort int, regAddresses []string, bindgRPCToService func(s grpc.ServiceRegistrar),
+	messageHandler func(method string, parameters []byte) (response proto.Message, err error)) (err error) {
 	Logger.Printf("Start(%s, %d)\n", serviceName, grpcListenPort)
 	// start the service
 	listeningAddress, grpcServer, startListening, err := startgRPC(grpcListenPort)
@@ -38,6 +113,14 @@ func Start(serviceName string, grpcListenPort int, regAddresses []string, bindgR
 	// make sure it registers to the registry service
 	unregister := registerAddress(serviceName, regAddresses, listeningAddress)
 	defer unregister()
+
+	// mq
+	if messageHandler != nil {
+		start_mq, listening_address_mq := bindMQToService(0, messageHandler)
+		listeningAddress = listening_address_mq // override the gRPC address with mq address
+		serviceName += "MQ"
+		go start_mq()
+	}
 
 	Logger.Printf("%s starts listening on %s\n", serviceName, listeningAddress)
 	startListening()

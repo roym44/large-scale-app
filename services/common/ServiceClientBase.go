581abe863659fb7ac6bebda039aabb3119a50693
Avishay Spitzer <a_spitzer@apple.com>
2024-08-04 14:14:11 +0300

    implement getMQNodes, ConnectMQ in service client base. to be tested

--- services/common/ServiceClientBase.go
+++ services/common/ServiceClientBase.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 
+	"github.com/pebbe/zmq4"
+
 	RegServiceClient "github.com/TAULargeScaleWorkshop/RLAD/services/reg-service/client"
 	. "github.com/TAULargeScaleWorkshop/RLAD/utils"
 	"google.golang.org/grpc"
@@ -13,6 +15,33 @@ type ServiceClientBase[client_t any] struct {
 	RegistryAddresses []string // to connect to the registry service
 	ServiceName       string   // to disover our server's nodes
 	CreateClient      func(grpc.ClientConnInterface) client_t
+}
+
+func (obj *ServiceClientBase[client_t]) getMQNodes() ([]string, error) {
+	regClient := RegServiceClient.NewRegServiceClient(obj.RegistryAddresses)
+	nodes, err := regClient.Discover(obj.ServiceName)
+	if err != nil {
+		Logger.Printf("Error calling Discover: %s", err)
+	}
+	return nodes, err
+}
+
+func (obj *ServiceClientBase[client_t]) ConnectMQ() (socket *zmq4.Socket, err error) {
+	nodes, err := obj.getMQNodes()
+	if err != nil {
+		Logger.Printf("Error calling getMQNodes: %s", err)
+	}
+	socket, err = zmq4.NewSocket(zmq4.REP)
+	if err != nil {
+		Logger.Fatalf("Failed to create a new zmq socket: %v", err)
+	}
+	for _, node := range nodes {
+		err = socket.Connect(node)
+		if err != nil {
+			Logger.Printf("Failed to connect a zmq socket: %v", err)
+		}
+	}
+	return socket, err
 }
 
 // randomly picks a service node address to connect to

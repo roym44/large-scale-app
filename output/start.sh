dd4705cea1d8916553e1fc34a9d0444246c6eb59
Roy Mayan <>
2024-08-10 17:28:36 +0300

    Uncommented

--- output/start.sh
+++ output/start.sh
@@ -17,20 +17,20 @@ main () {
     # ------------------------ RegService ------------------------
     echo "Starting registry services..."
     run_service "reg-service/service/RegServiceRoot.yaml" "RegService1(root)"
-    # run_service "reg-service/service/AnotherRegService.yaml" "RegService2"
-    # run_service "reg-service/service/AnotherRegService.yaml" "RegService3"
+    run_service "reg-service/service/AnotherRegService.yaml" "RegService2"
+    run_service "reg-service/service/AnotherRegService.yaml" "RegService3"
 
     # ------------------------ CacheService ------------------------
     echo "Starting cache services..."
     run_service "cache-service/service/CacheServiceRoot.yaml" "CacheService1(root)"
     run_service "cache-service/service/AnotherCacheService.yaml" "CacheService2"
-    # run_service "cache-service/service/AnotherCacheService.yaml" "CacheService3"
+    run_service "cache-service/service/AnotherCacheService.yaml" "CacheService3"
 
     # ------------------------ TestService ------------------------
     echo "Starting test services..."
-    # run_service "test-service/service/TestService.yaml" "TestService1"
-    # run_service "test-service/service/TestService.yaml" "TestService2"
-    # run_service "test-service/service/TestService.yaml" "TestService3"
+    run_service "test-service/service/TestService.yaml" "TestService1"
+    run_service "test-service/service/TestService.yaml" "TestService2"
+    run_service "test-service/service/TestService.yaml" "TestService3"
 }
 
 main
